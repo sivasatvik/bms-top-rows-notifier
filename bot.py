ecca58ec9d940caf3dd5c22b3403df7a034b5ff8
Siva Satvik <sivasatvik@gmail.com>
2023-07-15 13:22:52 +0530

    Update the script with latest version of the packages

--- bot.py
+++ bot.py
@@ -11,12 +11,14 @@ from email import encoders
 from pytz import timezone
 from datetime import datetime
 from selenium import webdriver
+import undetected_chromedriver as uc
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.header import decode_header
 from email.mime.multipart import MIMEMultipart
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -24,7 +26,7 @@ class Browser:
     def __init__(self, driver_path, site):
         # Initialize some useful strings to find the elements on the page
         self.prasads_large_screen = "//a[text()='Prasads Multiplex: Hyderabad']"
-        self.show_type            = "//div[text()='PCX']"
+        self.show_type            = "//div[text()='PCX DOLBY ATMOS']"
         self.accept_button        = "//div[@id='btnPopupAccept']"
         self.select_seats         = "//div[@id='proceed-Qty']"
         self.close_btn            = "//a[@id='dismiss']"
@@ -47,7 +49,8 @@ class Browser:
         self.init_option()
 
         # Initialize the actual browser that will be automated
-        self.browser = webdriver.Chrome(executable_path=driver_path, options=self.option)
+        self.service = Service(executable_path=driver_path)
+        self.browser = uc.Chrome(service=self.service, options=self.option)
         self.browser.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         self.browser.implicitly_wait(5)
         self.browser.get(site)
@@ -55,15 +58,15 @@ class Browser:
 
 
     def init_option(self):
-        self.option.add_experimental_option("excludeSwitches", ["enable-automation"])
-        self.option.add_experimental_option('useAutomationExtension', False)
+        # self.option.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # self.option.add_experimental_option('useAutomationExtension', False)
 
         self.option.add_argument('--disable-blink-features=AutomationControlled')
         # Add this option if you don't want the browser to be shown
-        self.option.add_argument("--headless")
+        # self.option.add_argument("--headless")
         # self.option.add_argument("start-maximized")
-        self.option.add_argument("window-size=1280,800")
-        self.option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+        # self.option.add_argument("window-size=1280,800")
+        # self.option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
     
     def wait_for_personalized_update_pop_up(self):
         try:
@@ -76,7 +79,7 @@ class Browser:
 
     def is_prasads_available(self):
         try:
-            prasads = self.browser.find_element_by_xpath(self.prasads_large_screen)
+            prasads = self.browser.find_element("xpath", self.prasads_large_screen)
             # print(prasads)
             return True
         except Exception as e:
@@ -87,12 +90,13 @@ class Browser:
     def open_and_find_seats_available(self):
         print("Finding seats available")
         try:
-            show_time_list = self.browser.find_elements_by_xpath(self.show_type)
+            show_time_list = self.browser.find_elements("xpath", self.show_type)
             for show_time in show_time_list:
                 self.browser.execute_script("arguments[0].scrollIntoView(false);", show_time)
                 show_time.click()
                 try:
-                    self.browser.find_element_by_xpath(self.accept_button).click()
+                    self.browser.find_element("xpath", self.accept_button).click()
+                    print("Accept button clicked")
                 except Exception as e:
                     print("Accept button not found")
                     print(e)
@@ -107,7 +111,7 @@ class Browser:
                 # print("Sleepig 5 seconds")
                 # sleep(5)
                 try:
-                    self.browser.find_element_by_xpath(self.close_btn).click()
+                    self.browser.find_element("xpath", self.close_btn).click()
                 except Exception as e:
                     print("Close button not found")
                     print(e)
@@ -118,24 +122,24 @@ class Browser:
     def find_best_seats(self):
         print("Finding seats in top rows")
         try:
-            layout = self.browser.find_element_by_xpath(self.seat_layout)
-            rows = [tr for tr in layout.find_elements_by_xpath(".//tr")]
+            layout = self.browser.find_element("xpath", self.seat_layout)
+            rows = [tr for tr in layout.find_elements("xpath", ".//tr")]
             # Skipping dummy row at the top
             rows = rows[1:]
             seat_map = {}
             send_mail = False
             for row in rows:
-                tds = [td for td in row.find_elements_by_xpath(".//td")]
-                row_id = tds[0].find_element_by_xpath(".//div").text
+                tds = [td for td in row.find_elements("xpath", ".//td")]
+                row_id = tds[0].find_element("xpath", ".//div").text
                 if((row_id != self.first_row) and (row_id != self.second_row) and (row_id != self.third_row)):
                     continue
-                seats = [div for div in tds[1].find_elements_by_xpath(".//div")]
+                seats = [div for div in tds[1].find_elements("xpath", ".//div")]
                 # Skipping whitespace coloumn
                 seats = seats[1:]
                 seats_available = []
                 for seat in seats:
-                    seat_number = seat.find_element_by_xpath(".//a").text
-                    status = seat.find_element_by_xpath(".//a").get_attribute("class")
+                    seat_number = seat.find_element("xpath", ".//a").text
+                    status = seat.find_element("xpath", ".//a").get_attribute("class")
                     # print("Seat: " + seat_number + ", status: " + status)
                     if(status == self.available):
                         seats_available.append(seat_number)
@@ -180,7 +184,7 @@ class Browser:
         message = ""
         message += "For the showtime: "
         try:
-            message += self.browser.find_element_by_xpath(self.show_time).text
+            message += self.browser.find_element("xpath", self.show_time).text
         except Exception as e:
             print("Cannot get show time")
             print(e)
@@ -248,6 +252,6 @@ if __name__ == "__main__":
     # https://in.bookmyshow.com/buytickets/ant-man-and-the-wasp-quantumania-3d-hyderabad/movie-hyd-ET00351697-MT/20230217
     while(True):
         main(site)
-        print("Sleeping for 10 mins")
-        sleep(600)
+        print("Sleeping for 5 mins")
+        sleep(300)
         ## TODO: Quit/stop the script for a particular show when email is already sent.
